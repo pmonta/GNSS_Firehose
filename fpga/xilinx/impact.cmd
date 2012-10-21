6c93947708bf8b734d68fde5eb28fdf35c68ad6c
Peter Monta <pmonta@gmail.com>
2012-10-21 09:49:53 -0700

    working picoblaze UART demo

--- fpga/xilinx/impact.cmd
+++ fpga/xilinx/impact.cmd
@@ -2,6 +2,6 @@ setMode -bs
 setCable -port auto
 Identify -inferir 
 identifyMPM 
-assignFile -p 1 -file "/home/pmonta/gps-front-end/fpga/xilinx/chip.bit"
+assignFile -p 1 -file "chip.bit"
 Program -p 1
 quit
