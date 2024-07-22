eaa1dbcb64e2d2a0c2e26848c2ee5b219438d111
Larry Doolittle <LRDoolittle@lbl.gov>
2024-07-21 17:51:23 -0700

    fpga/hdl/packet_rx.v: pipeline comparisons
    Still passes regression test
    Should make timing better on hardware

--- fpga/hdl/packet_rx.v
+++ fpga/hdl/packet_rx.v
@@ -34,24 +34,37 @@ module packet_rx(
   initial eth_rx_we=0;
   initial eth_rx_ready=0;
 
+  // Pipeline the comparisons used in the pattern match states
+  reg mm0=0, mm1=0, mm2=0, mm3=0, mm4=0, mm5=0, mm6=0, pv=0;
+  always @(posedge clk) begin
+    mm0 <= data==8'hd5;
+    mm1 <= data==mac_addr[47:40];
+    mm2 <= data==mac_addr[39:32];
+    mm3 <= data==mac_addr[31:24];
+    mm4 <= data==mac_addr[23:16];
+    mm5 <= data==mac_addr[15:8];
+    mm6 <= data==mac_addr[7:0];
+    pv <= ctl==2'b11;
+  end
+
   assign eth_rx_wdata = data;
 
   always @(posedge clk)
     case (state)
-      IDLE: if (ctl==2'b11) state <= PREAMBLE;
-      PREAMBLE: if (ctl!=2'b11) state <= IDLE; else if (data==8'hd5) state <= DEST_1;
-      DEST_1: if (ctl!=2'b11) state <= IDLE; else if (data==mac_addr[47:40]) state <= DEST_2; else state <= IGNORE;
-      DEST_2: if (ctl!=2'b11) state <= IDLE; else if (data==mac_addr[39:32]) state <= DEST_3; else state <= IGNORE;
-      DEST_3: if (ctl!=2'b11) state <= IDLE; else if (data==mac_addr[31:24]) state <= DEST_4; else state <= IGNORE;
-      DEST_4: if (ctl!=2'b11) state <= IDLE; else if (data==mac_addr[23:16]) state <= DEST_5; else state <= IGNORE;
-      DEST_5: if (ctl!=2'b11) state <= IDLE; else if (data==mac_addr[15:8]) state <= DEST_6; else state <= IGNORE;
-      DEST_6: if (ctl!=2'b11) state <= IDLE; else if (data==mac_addr[7:0]) begin c <= 0; state <= SKIP; end else state <= IGNORE;
+      IDLE: if (pv) state <= PREAMBLE;
+      PREAMBLE: if (~pv) state <= IDLE; else if (mm0) state <= DEST_1;
+      DEST_1: if (~pv) state <= IDLE; else if (mm1) state <= DEST_2; else state <= IGNORE;
+      DEST_2: if (~pv) state <= IDLE; else if (mm2) state <= DEST_3; else state <= IGNORE;
+      DEST_3: if (~pv) state <= IDLE; else if (mm3) state <= DEST_4; else state <= IGNORE;
+      DEST_4: if (~pv) state <= IDLE; else if (mm4) state <= DEST_5; else state <= IGNORE;
+      DEST_5: if (~pv) state <= IDLE; else if (mm5) state <= DEST_6; else state <= IGNORE;
+      DEST_6: if (~pv) state <= IDLE; else if (mm6) begin c <= 0; state <= SKIP; end else state <= IGNORE;
       SKIP:                        // skip over source MAC address and type/len field to get to start of payload
-        if (ctl!=2'b11)
+        if (~pv || (ctl!=2'b11))  // might be right
           state <= IDLE;
         else begin
           c <= c + 1;
-          if (c==4'd7) begin
+          if (c==4'd6) begin
             eth_rx_addr <= 0;
             eth_rx_we <= 1;
             state <= PAYLOAD;
